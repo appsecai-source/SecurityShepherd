0b28c2b19a5cdb5e1c0560b6b0cfb65d157d25db
appsecai-source <appsecai-source@users.noreply.github.com>
2026-04-21 01:52:52 +0000

    fix: Fortify Aviator remediation for vulnerability 292919967

--- src/main/java/servlets/module/challenge/SqlInjectionEscaping.java
+++ src/main/java/servlets/module/challenge/SqlInjectionEscaping.java
@@ -81,53 +81,53 @@ public class SqlInjectionEscaping extends HttpServlet {
         String ApplicationRoot = getServletContext().getRealPath("");
 
         log.debug("Getting Connection to Database");
-        Connection conn = Database.getChallengeConnection(ApplicationRoot, "SqlChallengeEscape");
-        Statement stmt = conn.createStatement();
-        log.debug("Gathering result set");
-        ResultSet resultSet =
-            stmt.executeQuery("SELECT * FROM customers WHERE customerId = '" + aUserId + "'");
+        try (Connection conn = Database.getChallengeConnection(ApplicationRoot, "SqlChallengeEscape");
+             Statement stmt = conn.createStatement();
+             ResultSet resultSet = stmt.executeQuery("SELECT * FROM customers WHERE customerId = '" + aUserId + "'")) {
+          
+          log.debug("Gathering result set");
 
-        int i = 0;
-        htmlOutput = "<h2 class='title'>" + bundle.getString("response.searchResults") + "</h2>";
-        htmlOutput +=
-            "<table><tr><th>"
-                + bundle.getString("response.table.name")
-                + "</th><th>"
-                + bundle.getString("response.table.address")
-                + "</th><th>"
-                + bundle.getString("response.table.comment")
-                + "</th></tr>";
-
-        log.debug("Opening Result Set from query");
-        while (resultSet.next()) {
-          log.debug("Adding Customer " + resultSet.getString(2));
+          int i = 0;
+          htmlOutput = "<h2 class='title'>" + bundle.getString("response.searchResults") + "</h2>";
           htmlOutput +=
-              "<tr><td>"
-                  + Encode.forHtml(resultSet.getString(2))
-                  + "</td><td>"
-                  + Encode.forHtml(resultSet.getString(3))
-                  + "</td><td>"
-                  + Encode.forHtml(resultSet.getString(4))
-                  + "</td></tr>";
-          i++;
+              "<table><tr><th>"
+                  + bundle.getString("response.table.name")
+                  + "</th><th>"
+                  + bundle.getString("response.table.address")
+                  + "</th><th>"
+                  + bundle.getString("response.table.comment")
+                  + "</th></tr>";
+
+          log.debug("Opening Result Set from query");
+          while (resultSet.next()) {
+            log.debug("Adding Customer " + resultSet.getString(2));
+            htmlOutput +=
+                "<tr><td>"
+                    + Encode.forHtml(resultSet.getString(2))
+                    + "</td><td>"
+                    + Encode.forHtml(resultSet.getString(3))
+                    + "</td><td>"
+                    + Encode.forHtml(resultSet.getString(4))
+                    + "</td></tr>";
+            i++;
+          }
+          htmlOutput += "</table>";
+          if (i == 0) {
+            htmlOutput = "<p>" + bundle.getString("response.noResults") + "</p>";
+          }
+        } catch (SQLException e) {
+          log.debug("SQL Error caught - " + e.toString());
+          htmlOutput +=
+              "<p>"
+                  + errors.getString("error.detected")
+                  + "</p>"
+                  + "<p>"
+                  + Encode.forHtml(e.toString())
+                  + "</p>";
+        } catch (Exception e) {
+          out.write(errors.getString("error.funky"));
+          log.fatal(levelName + " - " + e.toString());
         }
-        htmlOutput += "</table>";
-        if (i == 0) {
-          htmlOutput = "<p>" + bundle.getString("response.noResults") + "</p>";
-        }
-      } catch (SQLException e) {
-        log.debug("SQL Error caught - " + e.toString());
-        htmlOutput +=
-            "<p>"
-                + errors.getString("error.detected")
-                + "</p>"
-                + "<p>"
-                + Encode.forHtml(e.toString())
-                + "</p>";
-      } catch (Exception e) {
-        out.write(errors.getString("error.funky"));
-        log.fatal(levelName + " - " + e.toString());
-      }
       log.debug("Outputting HTML");
       out.write(htmlOutput);
     } else {
